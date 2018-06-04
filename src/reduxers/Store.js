4c0cad1650ef0b6eb62775df3e28aecc85e8380a
RONNY FREITES <ronnyangelo.freites@gmail.com>
2018-06-04 15:37:13 -0600

    Redux: Implementing combineReducers() from Scratch

--- src/reduxers/Store.js
+++ src/reduxers/Store.js
@@ -2,16 +2,6 @@ import { createStore, combineReducers } from 'redux';
 import todos from './Todos';
 import visibilityFilter from './VisibilityFilter';
 
-// Create a Redux store holding the state of your app.
-// Its API is { subscribe, dispatch, getState }.
-
-
-// In combineReducers function, if object notation key-value are the same
-const todoApp = combineReducers({
-  todos,
-  visibilityFilter
-});
-
 // const todoApp = (state = {}, action) => {
 //   return {
 //     todos: todos(
@@ -24,6 +14,29 @@ const todoApp = combineReducers({
 //     )
 //   };
 // };
+
+// const combineReducers = (reducers) => {
+//   return (state = {}, action) => {
+//     return Object.keys(reducers).reduce(
+//       (nextState, key) => {
+//         nextState[key] = reducers[key](
+//           state[key],
+//           action
+//         );
+//         return nextState;
+//       },
+//       {}
+//     );
+//   };
+// };
+
+// Create a Redux store holding the state of your app.
+// Its API is { subscribe, dispatch, getState }.
+// In combineReducers function, if object notation key-value are the same
+const todoApp = combineReducers({
+  todos,
+  visibilityFilter
+});
 
 const store = createStore(todoApp);
 
